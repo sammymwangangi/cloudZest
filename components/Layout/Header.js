8a317f6d83ff402fa2a6f89c3937e914f1e0fc98
Sammy <sammymwinzi2@gmail.com>
2023-07-10 18:53:53 +0300

    About us

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -125,24 +125,12 @@ const Header = () => {
             <div className="text-2xl font-medium text-black">CloudZest</div>
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
-            <LinkScroll
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              onSetActive={() => {
-                setActiveLink("about");
-              }}
-              className={
-                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
-                (activeLink === "about"
-                  ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 a")
-              }
+            <Link
+              href="/about"
+              className="px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative"
             >
               About
-            </LinkScroll>
+            </Link>
             <Popover className="relative">
               <Popover.Button
                 className={
---
+++ pages/about.js
@@ -0,0 +1,49 @@
+import React, { useMemo } from "react";
+import Image from "next/image";
+import Layout from "../components/Layout/Layout";
+import SeoHead from "../components/SeoHead";
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <SeoHead title="Cloud Computing" />
+      <Layout>
+        <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <nav className="flex items-center justify-center mb-5" aria-label="Breadcrumb">
+          <ol role="list" className="flex items-center space-x-4">
+            <li>
+              <div>
+                <Link href="/" className="text-red-400 hover:text-gray-500">
+                  Home
+                </Link>
+              </div>
+            </li>
+              <li>
+                <div className="flex items-center">
+                  <ChevronRightIcon
+                    className="h-5 w-5 flex-shrink-0 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <div className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    About
+                  </div>
+                </div>
+              </li>
+          </ol>
+        </nav>
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              About CloudZest
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+            We empower businesses like yours with cutting-edge solutions for growth, efficiency, and success.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
