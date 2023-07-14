539b045078b11df5e546a56f260facd91ad41973
Sammy <sammymwinzi2@gmail.com>
2023-07-14 14:28:46 +0300

    changes

--- components/Computing.js
+++ components/Computing.js
@@ -51,7 +51,7 @@ const Computing = () => {
                 advantage today.
               </p>
             </div>
-            <div className="lg:pr-4">
+            <div className="lg:pr-4 lg:pt-4">
               <Link href="/services/cloud-computing" className="py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none">
                 Learn more
             </Link>
