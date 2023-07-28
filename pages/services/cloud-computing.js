92ed13151bc312c386b6ba571d69ab1d88c83f3c
prosam <sammymwinzi2@gmail.com>
2023-07-28 06:32:25 +0300

    computing changes

--- pages/services/cloud-computing.js
+++ pages/services/cloud-computing.js
@@ -7,7 +7,13 @@ import ScrollAnimationWrapper from "../../components/Layout/ScrollAnimationWrapp
 import Hero from "../../components/Hero";
 import Layout from "../../components/Layout/Layout";
 import SeoHead from "../../components/SeoHead";
-import CloudTestimony from "../../components/CloudTestimony";
+import { GridList, GridListItem } from "../../components/GridList";
+import { GridPattern } from "../../components/GridPattern";
+import { SectionIntro } from "../../components/SectionIntro";
+import { Container } from "../../components/Container";
+import { FadeIn } from "../../components/FadeIn";
+// import CloudTestimony from "../../components/CloudTestimony";
+import clsx from "clsx";
 
 const features = [
   {
@@ -56,6 +62,36 @@ const features = [
       "Cloud Zest provides training and support services to help businesses effectively utilize cloud computing resources. We offer training programs to educate employees on cloud technologies, best practices, and security measures. Our dedicated support team is available to address any technical queries or issues that may arise.",
   },
 ];
+
+function CloudTestimony({ className }) {
+  return (
+    <div
+      className={clsx(
+        "relative isolate bg-neutral-50 py-16 sm:py-28 md:py-32",
+        className
+      )}
+    >
+      <GridPattern
+        className="absolute inset-0 -z-10 h-full w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_50%,transparent_60%)]"
+        yOffset={-256}
+      />
+      <Container>
+        <FadeIn>
+          <figure className="mx-auto max-w-4xl">
+            <blockquote className="relative font-display text-3xl font-medium tracking-tight text-neutral-950 sm:text-4xl">
+              <p className="before:content-['“'] after:content-['”'] sm:before:absolute sm:before:right-full">
+              By offering these comprehensive services, Cloud Zest enables businesses to leverage the full potential of cloud computing, empowering them with scalability, cost savings, security, and agility in today's competitive market.
+              </p>
+              <p className="text-2xl mt-4 text-orange-600">
+                Partner with Cloud Zest and experience the cloud advantage today.
+              </p>
+            </blockquote>
+          </figure>
+        </FadeIn>
+      </Container>
+    </div>
+  );
+}
 
 export default function CloudComputing() {
   return (
