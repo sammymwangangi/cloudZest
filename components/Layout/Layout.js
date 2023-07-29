f7cd9f7ac614a0c3b662253fb67e595be13839e0
prosam <sammymwinzi2@gmail.com>
2023-07-29 16:51:15 +0300

    floating button

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Header2 from "./Header2";
+import FloatingButtons from "/components/FloatingButtons";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header2 />
       {children}
+      <FloatingButtons/>
       <Footer />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,7 @@ export default function Home({ services }) {
         <WebsiteDevelopment/>
         <UiUx/>
         
+        
       </Layout>
     </>
   );
